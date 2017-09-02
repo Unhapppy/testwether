1fa6f9e9c78fe9d25368d920f986fab073a355dc
Unhapppy <951970312@qq.com>
2017-09-02 19:10:22 +0800

    First commit.

---
+++ app/src/main/java/android/testweather/com/testweather/MainActivity.java
@@ -0,0 +1,13 @@
+package android.testweather.com.testweather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
