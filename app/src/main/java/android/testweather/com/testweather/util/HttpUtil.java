c16c13fd3109a61722a249b2ba91a9a0178fbca1
Unhapppy <951970312@qq.com>
2017-09-03 16:01:21 +0800

    add the query function of province/city/county three level list

---
+++ app/src/main/java/android/testweather/com/testweather/util/HttpUtil.java
@@ -0,0 +1,18 @@
+package android.testweather.com.testweather.util;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by Administrator on 2017/9/2 0002.
+ */
+
+public class HttpUtil {
+
+    public static void sendOkHttpRequest(String address, okhttp3.Callback callback) {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
+
+}
