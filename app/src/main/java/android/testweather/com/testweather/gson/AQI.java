536f6834658c3e4b7818ab1b683becfe0e4195ea
Unhapppy <951970312@qq.com>
2017-09-04 17:48:02 +0800

    finish demo

---
+++ app/src/main/java/android/testweather/com/testweather/gson/AQI.java
@@ -0,0 +1,19 @@
+package android.testweather.com.testweather.gson;
+
+/**
+ * Created by Administrator on 2017/9/3 0003.
+ */
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity{
+
+        public String aqi;
+
+        public String pm25;
+
+    }
+
+}
--- app/src/main/java/android/testweather/com/testweather/util/Utility.java
+++ app/src/main/java/android/testweather/com/testweather/util/Utility.java
@@ -3,7 +3,10 @@ package android.testweather.com.testweather.util;
 import android.testweather.com.testweather.db.City;
 import android.testweather.com.testweather.db.County;
 import android.testweather.com.testweather.db.Province;
+import android.testweather.com.testweather.gson.Weather;
 import android.text.TextUtils;
+
+import com.google.gson.Gson;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -74,6 +77,19 @@ public class Utility {
             }
         }
         return false;
+    }
+
+    public static Weather handleWeatherResponse(String response) {
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather");
+            String weatherContent = jsonArray.getJSONObject(0).toString();
+            return new Gson().fromJson(weatherContent, Weather.class);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
 }
